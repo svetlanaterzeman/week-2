6c46dcc9438006c31cd435d7d9ebad1a41d80817
svetlanaterzeman <75183117+svetlanaterzeman@users.noreply.github.com>
2020-12-08 17:58:14 +0300

    Add files via upload

---
+++ Неделя№2 Задание 17 из ЕГЭ №5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задание_17_из_ЕГЭ
+{
+    class Задание5    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int max = 0;
+            for (int i = 29384; i < 48494; i++)
+            {
+                if ((i % 78 == 0) && (i % 7 != 0) && (i % 8 != 0) && (i % 12 != 0) && (i % 80 != 0))
+                {
+                    counter++;
+                    max = i;
+                }
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
