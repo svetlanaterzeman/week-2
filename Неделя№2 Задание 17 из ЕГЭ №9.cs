262ee22a1263a2fffecd6871b06e9769826f7b3d
svetlanaterzeman <75183117+svetlanaterzeman@users.noreply.github.com>
2020-12-08 18:51:45 +0300

    Add files via upload

---
+++ Неделя№2 Задание 17 из ЕГЭ №9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Задание_17_из_ЕГЭ
+{
+    class Задание9   {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int min = 1000000000;
+            for (int i = 3578; i < 8010; i++)
+            {
+                if((i%17==0)&&(i%93!=0)&&(i%100==11))
+                {   
+                    if(min>i)
+                    {
+                        min = i;
+                    }
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
