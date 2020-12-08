b7568d870ded802fd5623432f911c05d72faa5e2
svetlanaterzeman <75183117+svetlanaterzeman@users.noreply.github.com>
2020-12-08 17:36:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задание_17_из_ЕГЭ
+{
+    class Задание1    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int max = 0;
+            for (int i = 2077; i < 8277; i++)
+            {
+                if ((i % 6 == 0) && (i % 4 != 0) && (i % 9 != 0) && (i % 20 != 0) && (i % 36 != 0))
+                {
+                    counter++;
+                    max = i;
+                }
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
