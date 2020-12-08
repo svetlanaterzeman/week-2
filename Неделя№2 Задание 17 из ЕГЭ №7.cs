0ede6c9c8d2384886f272e1bdab7b0c1dba06563
svetlanaterzeman <75183117+svetlanaterzeman@users.noreply.github.com>
2020-12-08 18:08:42 +0300

    Add files via upload

---
+++ Неделя№2 Задание 17 из ЕГЭ №7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задание_17_из_ЕГЭ
+{
+    class Задание7    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int predmax = 0;
+            for (int i = 2765; i < 9526; i++)
+            {
+                if ((i % 205 == 0) && (i % 10 != 0) && (i % 16 != 0) && (i % 82 != 0))
+                {
+                    predmax = max;
+                    max = i;
+                }
+            }
+            Console.WriteLine(predmax);
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
