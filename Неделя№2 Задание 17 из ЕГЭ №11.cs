4b91116d16d4de5f1500427a4e27ef6dd31a1ffb
svetlanaterzeman <75183117+svetlanaterzeman@users.noreply.github.com>
2020-12-08 19:58:31 +0300

    Add files via upload

---
+++ Неделя№2 Задание 17 из ЕГЭ №11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Задание_17_из_ЕГЭ
+{
+    class Задание11   {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int max = 0;
+            for (int i = 73; i < 5579; i++)
+            {
+                if(157080%i==0)
+                {
+                    counter++;
+                    if(i%10==0)
+                    {
+                        max = i;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
