a58062de4dce4f2c57e2726b1648bd418831c29e
svetlanaterzeman <75183117+svetlanaterzeman@users.noreply.github.com>
2020-12-08 18:01:52 +0300

    Add files via upload

---
+++ Неделя№2 Задание 17 из ЕГЭ №6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Задание_17_из_ЕГЭ
+{
+    class Задание6    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int max = 0;
+            for (int i = 1234; i < 8766; i++)
+            {
+                if ((i % 27 == 0) && (i % 4 != 0) && (i % 72 != 0) && (i % 81 != 0) && (i % 128 != 0))
+                {
+                    counter++;
+                    if((i%7==3)&&(i>max))
+                    {
+                        max = i;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
