77b6556f824324228328c7efd3411f31565e0c30
svetlanaterzeman <75183117+svetlanaterzeman@users.noreply.github.com>
2020-12-08 17:53:31 +0300

    Add files via upload

---
+++ Неделя№2 Задание 17 из ЕГЭ №4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задание_17_из_ЕГЭ
+{
+    class Задание3    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int max = 0;
+            for (int i = 10000; i < 100001; i++)
+            {
+                if ((i % 23 == 0) && (i % 5 != 0) && (i % 6 != 0) && (i % 7 != 0) && (i % 8 != 0))
+                {
+                    counter++;
+                    max = i;
+                }
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
